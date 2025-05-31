df363ce326ea68274479254e9ed58d3393aeb591
Armando <ar.maldonadop02@gmail.com>
2025-05-31 12:20:33 -0400

    Updated 04_variables.py. Added some explanations

--- 01_basic/04_variables.py
+++ 01_basic/04_variables.py
@@ -8,46 +8,53 @@ from os import system
 if system("clear") != 0: system("cls")
 
 # Para asignar una variable solo hace falta poner el nombre de la variable y asignarle un valor
-my_name = "midudev"
+my_name = "bart"
 print(my_name)  # Imprime el valor de la variable my_name
 
-age = 32
+age = 40
 print(age)  # Imprime el valor de la variable age)
 
 # Reasignar un nuevo valor a una variable existente
-age = 39
+age = 23
 print(age)  # Ahora la variable age tiene el valor 39
 
 # Tipado dinámico: el tipo de dato se determine en tiempo de ejecución
 # No es necesario declarar explícitamente el tipo de variable
-name = "midudev"
+# Además de que no es necesario declarar el tipo de variable al momento de asignarla, se puede cambiar el tipo de dato de una variable en cualquier momento
+
+name = "tuck"
 print(type(name))  # Muestra el tipo de dato de la variable name (str)
 
-name = 32
+name = 22
 print(type(name))  # Ahora la variable tiene un número entero (int)
 
-# Tipado fuerte: Python no realiza conversione de tipo automáticas
+# Tipado fuerte: Python no realiza conversiones de tipo automáticas
 # Esto generará un error porque no se puede sumar un número con una cadena
 # print(10 + "2")  # ❌ TypeError: unsupported operand type(s) for +: 'int' and 'str'
 
 # f-string (literal de cadena de formato)
+# Se coloca una f antes de la cadena y se pueden insertar variables dentro de llaves {}. Además se pueden hacer operaciones con los int
 # desde la versión Python 3.6
 print(f"Hola {my_name}, tengo {age + 5} años")
 
-# No recomendada forma de asignar variables
+# Se pueden declarar varias variables en una misma línea, separando por comas las variables y los valores. 
+# ❌ No recomendada forma de asignar variables
 name, age, city = "midudev", 32, "Bogotá"
 
 # Convenciones de nombres de variables
 mi_nombre_de_variable = "ok" # snake_case
 nombre = "ok"
 
-miNombreDeVariable = "no-recomendado" # camelCase
-MiNombreDeVariable = "no-recomendado" # PascalCase
-minombredevariable = "no-recomendado" # todojunto
+miNombreDeVariable = "no-recomendado" # ❌ camelCase
+MiNombreDeVariable = "no-recomendado" # ❌ PascalCase
+minombredevariable = "no-recomendado" # ❌ todojunto
 
 mi_nombre_de_variable_123 = "ok"
 
+# En python no existen las constantes como tal, pero se pueden simular usando mayúsculas. Esta es la convención.
 MI_CONSTANTE = 3.14 # UPPER_CASE -> constantes
+# Igualmente al no ser constantes, se pueden cambiar su valor, pero no es recomendable hacerlo.
+
 
 # Nombres NO válidos de variables (esto generaría errores)
 # 123123_variable = "ko"  # ❌ No puede comenzar con un número
@@ -65,9 +72,9 @@ MI_CONSTANTE = 3.14 # UPPER_CASE -> constantes
 # 'return', 'try', 'while', 'with', 'yield']
 
 # Anotaciones de tipo (opcional, para mayor claridad en el código)
+# Se coloca el nombre de la variable seguido de dos puntos y el tipo de dato esperado
 is_user_logged_in: bool = True # Indica que la variable es un booleano
 print(is_user_logged_in)
 
 name: str = "midudev" # Indica que la variable es una cadena de texto
 print(name)
-
