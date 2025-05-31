1d3624679d10ca06d0b06d1287cc7c592d1ee7e2
Armando <ar.maldonadop02@gmail.com>
2025-05-31 09:58:18 -0400

    Updated file, minor changes

--- 01_basic/03_cast.py
+++ 01_basic/03_cast.py
@@ -19,6 +19,7 @@ print(type(float("3.1416")))  # Convierte "3.1416" a float y muestra su tipo. Re
 
 # Convertir un número decimal a entero (se trunca la parte decimal)
 print(int(3.1416))  # Convierte 3.1416 a 3 eliminando la parte decimal. Resultado: 3
+print(int(3.9999))  # También convierte 3.9999 a 3. Resultado: 3. Nunca se redondea, ya que un casting no realiza operaciones matemáticas.
 
 # Evaluar valores numéricos como booleanos
 print(bool(3))  # Cualquier número distinto de 0 es True. Resultado: True
@@ -31,7 +32,10 @@ print(bool(" "))  # Una cadena con espacios es True. Resultado: True
 print(bool("False"))  # Una cadena con texto, aunque sea "False", es True. Resultado: True
 
 # Redondear un número decimal
+print(round(2.49))  # Redondea 2.49 al entero más cercano. Resultado: 2
+print(round(2.5))  # Cuando el redondeo se hace con un .5, se redondea al entero par más cercano. Resultado: 2
 print(round(2.51))  # Redondea 2.51 al entero más cercano. Resultado: 3
 
 # Este genera un error y se comenta para evitar conflicto en la ejecución
+# No se puede convertir una cadena que no representa un número a un entero. Generará un ValueError.
 # print(int("Hola mundo"))  # ❌ Esto generaría un ValueError porque "Hola mundo" no es un número
